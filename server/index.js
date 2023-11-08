cbdc75cce1a1c9e0bb330822552e37932ec7c0f2
AnesioSousa <anesios98@gmail.com>
2023-11-08 20:53:43 -0300

    feat: Get all todos

--- server/index.js
+++ server/index.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
 
-app.use("/", (req, res) => {
-  return res.status(200).json({
-    message: "Hello World!",
-  });
+//middleware
+app.use(cors());
+
+app.use(express.json());
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.status(200).json(newTodo.rows[0]);
+  } catch (err) {
+    console.err(err.message);
+    return res.status(400).json({
+      message: `${err.message}`,
+    });
+  }
 });
+
+//get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const todoList = await pool.query("SELECT * FROM todo");
+    res.status(200).json(todoList.rows);
+  } catch (error) {
+    console.error(err);
+    res.status(500);
+  }
+});
+
+//update a todos
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
